389c28a4d118096da8c75621075cffabddbad8ec
BNQ2k <benjamin.lepisto@metropolia.fi>
2025-08-21 11:41:45 +0300

    git/commit

--- 2_6_Lepistö.py
+++ 2_6_Lepistö.py
@@ -1,11 +1,16 @@
 import random
 
+uno = random.randint(0, 9)
+dos = random.randint(0, 9)
+tres = random.randint(0, 9)
+kolmenumeroinen = str(uno) + str(dos) + str(tres)
 
+four = random.randint(1, 6)
+five = random.randint(1, 6)
+six = random.randint(1, 6)
+seven = random.randint(1, 6)
 
+nelinumeroinen = str(four) + str(five) + str(six) + str(seven)
 
-
-
-arpoo ja tulostaa kaksi erilaista numerolukon koodia:
-kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
-nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
-uwu
+print("Kolmenumeroinen lukon koodi 0-9: " + kolmenumeroinen)
+print("Nelinumeroinen lukon koodi 1-6: " + nelinumeroinen)
