0dc9d33a4b7eed7d7ffe288d80bce544e34e7587
BNQ2k <benjamin.lepisto@metropolia.fi>
2025-08-25 09:55:13 +0300

    git/commit

--- 3_3.py
+++ 3_3.py
@@ -1,9 +1,22 @@
 sukupuoli = input("Syötä biologinen sukupuoli (mies/nainen): ")
 sukupuoli = sukupuoli.lower()
 
+# mies
 if sukupuoli == "mies":
     mieshemoglobiini = int(input("Syötä miehen hemoglobiiniarvo: "))
-    if mieshemoglobiini < 135:
-        print("Hemoglobiiniarvo on liian alhainen. Mies voi lahjoittaa verta.")
+    if mieshemoglobiini < 134:
+        print("Hemoglobiiniarvo on liian alhainen.")
+    elif mieshemoglobiini <= 195:
+        print("Hemoglobiiniarvo on normaali.")
+    else:
+        print("Hemoglobiiniarvo on liian korkea.")
 
-    elif mieshemoglobiini > 135:
+# nainen
+if sukupuoli == "nainen":
+    naishemoglobiini = int(input("Syötä naisen hemoglobiiniarvo: "))
+    if naishemoglobiini < 117:
+        print("Hemoglobiiniarvo on liian alhainen.")
+    elif naishemoglobiini <= 175:
+        print("Hemoglobiiniarvo on normaali.")
+    else:
+        print("Hemoglobiiniarvo on liian korkea.")
